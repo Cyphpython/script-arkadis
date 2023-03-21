c91342b4f897cbfe466433e0b9b49914937fda8a
Elden Modder <90405810+Cyphpython@users.noreply.github.com>
2023-03-21 21:35:19 +0100

    change some css + add fxmanifest

---
+++ loading arkadis/LoadingArkadis/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "adamant"
+game 'gta5'
+
+author 'Elden Modder'
+
+Desciption 'A simple loadingscreen'
+
+files {
+    "./arkadis.html",
+    "./arkadis.css",
+    "./arkadis.js",
+    "./reduce logo fox.png"
+}
+
+loadscreen './arkadis.html'
