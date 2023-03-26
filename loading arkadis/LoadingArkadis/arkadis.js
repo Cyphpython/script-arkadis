b51e3311449c989acfa6c17c4f94aaf5a36f4e9f
Elden Modder <90405810+Cyphpython@users.noreply.github.com>
2023-03-26 18:21:51 +0200

    Delete loading arkadis directory

--- loading arkadis/LoadingArkadis/arkadis.js
+++
@@ -1,9 +0,0 @@
-var lien = document.getElementById("github")
-
-lien.addEventListener("mouseover", function() {
-    lien.style.color = "red";
-});
-
-lien.addEventListener("mouseout", function(){
-    lien.style.color = "";
-});
